546de33fd76fd93303ff03453dfcfc01f5c3d291
igordurigon <igor.durigon@grupond.com.br>
2026-02-20 09:57:53 -0300

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
